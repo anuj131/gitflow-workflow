bacc52658c32ab293ca1e2b747694a9d97e60c7b
anuj131 <anujkvn@gmail.com>
2020-03-31 20:55:32 +0530

    feature 2 commit

--- production-file.cpp
+++ production-file.cpp
@@ -4,10 +4,15 @@ class ProductionFile{
     void prodMethod() {
       cout << "Hello World!";
     }
+  public:
+    void feature2() {
+      cout << "feature 2 data";
+    }
 }
 
 int main() {
   ProductionFile pObj;
   pObj.prodMethod();
+  pObj.feature2();
   return 0;
 }
